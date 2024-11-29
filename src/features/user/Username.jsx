3dc065a2b836fc4daac9a5b310484a22cbfa97b7
Eduardo Escamilla <eduardo.escamillasal@gmail.com>
2024-11-29 11:18:19 -0600

    added flex and grid

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="hidden text-sm font-semibold">Eduardo</div>;
+  return <div className="hidden text-sm font-semibold md:block">Eduardo</div>;
 }
 
 export default Username;
