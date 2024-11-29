b5accb6bfa0647c9f5fae014b425c92d2c0412c0
Eduardo Escamilla <eduardo.escamillasal@gmail.com>
2024-11-29 11:39:55 -0600

    create order fix

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -108,9 +108,8 @@ export async function action({ request }) {
 
   if (Object.keys(errors).length > 0) return errors;
 
-  // const newOrder = await createOrder(order);
-  // return redirect(`/order/${newOrder.id}`);
-  return null;
+  const newOrder = await createOrder(order);
+  return redirect(`/order/${newOrder.id}`);
 }
 
 export default CreateOrder;
